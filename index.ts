ea684b331cc6121bac628221ad3965f203d56950
Vineet <infinite2202@gmail.com>
2025-04-05 16:53:42 +0530

    revising typescript

---
+++ index.ts
@@ -0,0 +1,11 @@
+// string
+let name1: string = "Vineet";
+console.log(name1);
+
+// number
+let num: number = 123;
+console.log(num);
+
+//  Boolean
+let val: boolean = true || false;
+console.log(val);
